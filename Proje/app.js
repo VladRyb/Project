d8f98827b5dda4032e044b9e5f73deb346641f88
VladRyb <60558646+VladRyb@users.noreply.github.com>
2020-05-21 19:34:18 +0300

    Update app.js

--- Proje/app.js
+++ Proje/app.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/user');
 const curerRouter = require('./routes/curer');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 
