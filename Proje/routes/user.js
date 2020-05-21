ed5bc200beb6d2a73ba5edeefa01a1d0486b4bc8
Mansur <baymns10@gmail.com>
2020-05-21 14:54:06 +0300

    Release1 Add main page with orderlists and show details

--- Proje/routes/user.js
+++ Proje/routes/user.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
 // const User = require('../mock');
-const user = { name: 'Admin', password: '123' };
+const user = {
+  name: 'Дима',
+  phone: 89161231212,
+  email: 'd@mail.ru',
+  password: '123'
+};
 const usersArr = [];
-
+const orders = [{
+    id: '321',
+    title: "Заказ MacDonald's",
+    positions: [{
+      position: 'Coca-Cola',
+      quantity: 2
+    }, {
+      position: 'Bit Testy',
+      quantity: 2
+    }], // [{position: burger, quantity: 2}, ...]
+    price: '300 руб',
+    address: 'Бауманская, 10',
+    comment: 'Была картошка, но я ее съел',
+    date: new Date().toLocaleDateString()
+  },
+  {
+    id: '123',
+    title: "Заказ KFC",
+    positions: [{
+      position: 'Pepsi',
+      quantity: 2
+    }, {
+      position: 'De Luxe',
+      quantity: 2
+    }], // [{position: burger, quantity: 2}, ...]
+    price: '250 руб',
+    address: 'Бауманская, 53',
+    date: new Date().toLocaleDateString()
+  }
+]
 router.get('/', (req, res) => {
   res.redirect('user/login');
 });
@@ -18,7 +52,6 @@ router.post('/login', async (req, res) => {
     //   email: req.body.email,
     //   password: req.body.password,
     // });
-    console.log(req.body.password);
     if (user.password == req.body.password) {
       res.redirect('/user/main'); //'http://localhost:3000/user/main'
     } else {
@@ -45,7 +78,7 @@ router.post('/signup', async (req, res) => {
     //   email: req.body.email,
     //   password: req.body.password,
     // });
-    let previousLength = usersArr;
+    let previousLength = usersArr.length;
     usersArr.push({
       name: req.body.name,
       phone: req.body.phone,
@@ -69,7 +102,17 @@ router.post('/signup', async (req, res) => {
 });
 
 router.get('/main', async (req, res) => {
-  res.render('user/main');
+  // console.log(orders)
+  res.render('user/main', {
+    user: user,
+    orders: orders
+  });
+});
+
+router.get('/show-details/:id', async (req, res) => {
+  console.log(req.params.id)
+  const order = orders.find(order => order.id == req.params.id)
+  res.render('user/show', order)
 });
 
 module.exports = router;
